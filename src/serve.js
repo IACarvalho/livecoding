59d7d41aff60e30d5f10835a8f38eeb4845e053a
Israel Afonso de Carvalho Neto <israelcarvalhodesenvolvedor@gmail.com>
2023-03-18 21:35:43 -0300

    Hello world

--- src/serve.js
+++ src/serve.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+import Fastify from 'fastify'
+
+const app = Fastify({
+  logger: true
+})
+
+app.get('/', (request, reply) => {
+  return reply.send({ message: "Hello world!"})
+})
+
+app.listen({ port: 3333}, (err, address) => {
+  if (err) {
+    app.log.error(err)
+    process.exit(1)
+  }
+})
